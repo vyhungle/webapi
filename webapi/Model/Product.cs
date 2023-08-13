a233d147ef18f2c13c1101d84c9fc2f58ec191d4
Le Nguyen Hung Vy <vyhungle@165112-LNHVy-Mac.local>
2023-08-13 14:51:28 +0700

    a

---
+++ webapi/Model/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Model
+{
+	[Table("product")]
+	public class Product
+	{
+		[Key, Required]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+	}
+}
+
